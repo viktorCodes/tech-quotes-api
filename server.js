692c669bff6fb75ddc3e0a68115ba5c167a95715
calimendoza <mbaivictor1@gmail.com>
2022-11-02 00:06:56 -0700

    adding post request for adding new quotes

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.get('api/quotes', (req, res) => {
 })
 
 
+//Post for adding new quotes
+
+app.post('api/quote', (req,res) => {
+   const newQuote =  req.query.quote;
+   const newPerson = req.query.person;
+
+   if(newQuote != '' && newPerson != ''){
+    quotes.push({quote: newQuote, person: newPerson});
+    res.send({quote:{quote: newQuote, person: newPerson}})
+   } else {
+    res.status(400).send()
+   }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server active on ${PORT}`)
