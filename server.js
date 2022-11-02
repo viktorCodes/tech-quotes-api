6824d095d5e417da396b143b1c4b1e99fb8eeff8
calimendoza <mbaivictor1@gmail.com>
2022-11-01 23:08:00 -0700

    adding get request for random quotes

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ const app = express();
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'));
 
+//Get Random Quote
+app.get('api/quotes/random', (req, res,) => {
+
+     const randomQuote = getRandomElement(quotes)
+
+     res.send({quote: randomQuote})
+    
+});
+
+app.get('api/quotes', (req, res, next) => {
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server active on ${PORT}`)
+})
